6ed4a41e236b01dedf1b5abba38ac62b6e5898af
megaton111 <lkh890111@gmail.com>
2021-07-28 17:52:49 +0900

    head meta 정보 추가

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,36 @@ export default {
   components: {
     SideBar,
   },
+  data(){
+    return {
 
-  data: () => ({
-    //
-  }),
+    }
+  } ,
+
+  metaInfo: {
+    title: '회사원이 부업으로 돈 벌 수 있는 방법을 소개합니다.',
+    titleTemplate: '%s | SecondJob',
+    htmlAttrs: {
+      // lang: window.getApp.$i18n.locale 
+      // i18n 사용하지 않는 경우 직접 입력
+    },
+    meta: [
+      { charset: 'utf-8'},
+      { name: 'description', content: '해외구매대행,위탁판매,쿠팡파트너스 퇴근 후 직장인이 부업으로 돈 벌수 있는 방법을 소개합니다.', vmid: 'description'},
+      { name: 'keywords', content: "해외구매대행,위탁판매,쿠팡파트너스,직장인부업,회사원부업,퇴근후부업"},
+      { name: 'author', content: "SecondJob"},
+      {
+        property: 'og:title',
+        content: '회사원이 부업으로 돈 벌 수 있는 방법을 소개합니다.',
+        template: chunk => `${chunk} | SecondJob`,
+        vmid: 'og:title'
+      },
+      // mobile
+      { name: 'viewport', content: 'width=device-width, initial-scale=1'}
+    ]
+  }
+
+
+
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import router from './router' ;
+import router from './router' 
+import Meta from 'vue-meta'
+
 
 import "@/assets/scss/_index.scss" ; // global scss
 
 Vue.config.productionTip = false
+Vue.use(Meta)
 
 new Vue({
   router ,
-  vuetify,
+  vuetify ,
   render: h => h(App)
 }).$mount('#app')
